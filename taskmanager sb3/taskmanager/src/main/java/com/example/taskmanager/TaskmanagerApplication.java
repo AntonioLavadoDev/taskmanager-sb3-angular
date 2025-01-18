b6e4f3cb34e285bc8be3a150ffaa67f86188030f
Antonio Lav <antoniolavadodev@gmail.com>
2025-01-18 10:30:02 +0100

    feat: add /tasks endpoint and initialize database

--- taskmanager sb3/taskmanager/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
+++ taskmanager sb3/taskmanager/src/main/java/com/example/taskmanager/TaskmanagerApplication.java
@@ -1,13 +1,29 @@
 package com.example.taskmanager;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.taskmanager.models.Task;
+import com.example.taskmanager.repositories.TaskRepository;
 
 @SpringBootApplication
 public class TaskmanagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskmanagerApplication.class, args);
+	}
+
+	
+	//Agregar tareas iniciales al arrancar la aplicación
+	@Bean
+	public CommandLineRunner init(TaskRepository taskRepository){
+		return args -> {
+			taskRepository.save(new Task(0, "Task 1", "Description 1", false));
+			taskRepository.save(new Task(0, "Task 2", "Description 2", false));
+			taskRepository.save(new Task(0, "Task 3", "Description 3", false));
+		};
 	}
 
 }
--- taskmanager sb3/taskmanager/src/main/java/com/example/taskmanager/controllers/TaskController.java
+++ taskmanager sb3/taskmanager/src/main/java/com/example/taskmanager/controllers/TaskController.java
@@ -1,14 +1,32 @@
 package com.example.taskmanager.controllers;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.taskmanager.models.Task;
+import com.example.taskmanager.repositories.TaskRepository;
 
 @RestController
 public class TaskController {
 
+    private final TaskRepository taskRepository;
+
+    // Constructor para inyectar el repositorio
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
     @GetMapping("/")
     public String welcome() {
         return "Welcome to the Task Manager";
+    }
+
+    // Endpoint para obtener todas las tareas
+    @GetMapping("/tasks")
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
     }
 
 }
